b1ec56c4453a84437186de069870372f076a8e41
João Bruno <brunofreitas.93@hotmail.com>
2020-08-08 23:18:15 -0300

    Validação Sintática com Bean Validation

--- src/main/java/com/joaobruno/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/joaobruno/cursomc/dto/CategoriaDTO.java
@@ -2,18 +2,23 @@ package com.joaobruno.cursomc.dto;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
 import com.joaobruno.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
+
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser de 5 e 80 caracteres")
 	private String nome;
-	
-	public CategoriaDTO() {		
+
+	public CategoriaDTO() {
 	}
-	
+
 	public CategoriaDTO(Categoria obj) {
 		id = obj.getId();
 		nome = obj.getNome();
@@ -33,6 +38,6 @@ public class CategoriaDTO implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
-	}	
-	
+	}
+
 }
--- src/main/java/com/joaobruno/cursomc/services/CategoriaService.java
+++ src/main/java/com/joaobruno/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.joaobruno.cursomc.domain.Categoria;
+import com.joaobruno.cursomc.dto.CategoriaDTO;
 import com.joaobruno.cursomc.repositories.CategoriaRepository;
 import com.joaobruno.cursomc.services.exceptions.DataIntegrityException;
 import com.joaobruno.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,5 +57,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
